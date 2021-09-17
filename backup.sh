f2ed7f3797208207f720eff00b91be7f26e131fb
root <root@bikini.davehasroot.com>
2021-09-17 15:30:17 -0600

    it works, but its dirty

--- backup.sh
+++ backup.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
 
-#Where to put the files before uploading to Drive
-DEST='/backup/site-backups'
+#Where to put the files
+DEST='/backup/site-backups/'
 #Archive file name
 DATE=$(date +%F)
 HOSTNAME=$(hostname -s)
-ARCHIVE="$hostname-$date.tar.gz"
+ARCHIVE="$HOSTNAME-$date.tar.gz"
+#with the following list of domains:
+awk '$1 == "ServerAlias" {print $2}' /etc/httpd/sites-enabled/*.conf | uniq > /tmp/domains.txt
+#echo "cat /tmp/domains.txt"
+#cat /tmp/domains.txt
 
 ###Make the sql bups, since I only have WP sites and HTML sites, I'll only worry about wordpress dbs here for now###
 #Make an array of domain names that have wp-config.php files currently and save domains and configs for later
@@ -17,12 +21,17 @@ HOST="localhost"
 mapfile -t DB_NAMES < <(awk -F"'" '$2 ~ /DB_N/ {print $4}' ${WP_CONFIGS})
 mapfile -t DB_USERS < <(awk -F"'" '$2 ~ /DB_U/ {print $4}' ${WP_CONFIGS})
 mapfile -t DB_PASSWORDS < <(awk -F"'" '$2 ~ /DB_P/ {print $4}' ${WP_CONFIGS})
-#Make the actual .sql dump files to our backup directory
+##Make the actual .sql dump files to our backup directory
 for ((i=0;i<${#DB_NAMES[@]};++i)); do
     mysqldump -h $HOST -u "${DB_USERS[i]}" -p"${DB_PASSWORDS[i]}" "${DB_NAMES[i]}" >> $DEST/${DOMAINS[i]}-$DATE.sql
 done
 
 #Backup the other files for each domain
-for domain in $(cat /var/userdata/domlist) ; do
-        tar -czf $DEST/$domain-$DATE.tar.gz /var/www/$domain 2>/dev/null
+#while read $domain ; do
+#	tar -czf $DEST/$domain-$DATE.tar.gz /var/www/$domain
+#echo "$domain"
+#done < /tmp/domains.txt
+
+for domain in $(cat /tmp/domains.txt) ; do
+	tar -czf $DEST/$domain-$DATE.tar.gz /var/www/$domain
 done
